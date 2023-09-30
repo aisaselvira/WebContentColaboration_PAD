adec7b36f498b9622ba333e6397a8ee0b0055915
Aisa Selvira <aisaselvira2020@gmail.com>
2023-09-30 19:07:01 +0700

    updatee

---
+++ public/js/script.js
@@ -0,0 +1,10 @@
+
+let dateInput = document.getElementById("date");
+dateInput.addEventListener("focus", function() {
+  this.type = "date";
+});
+
+// Code untuk mengaktifkan upload gambar pada input Photo
+let photoInput = document.getElementById("photo");
+photoInput.addEventListener("change", function() {
+});
